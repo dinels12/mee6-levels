4369766d8642fe6c3f35eeba868315f3f980e9ed
“dinels” <dinels12@gmail.com>
2021-11-07 15:59:01 -0400

    updated

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,19 @@ const { baseURL } = config['production'];
 
 class InitializeApp {
   private baseURL: string;
+  /**
+   * Get the users from a LeaderBoard ID.
+   *
+   * @param leaderBoardId id of the Leaderboard to get the data.
+   *
+   */
   constructor(leaderBoardId: string) {
     this.baseURL = `${baseURL}/${leaderBoardId}`;
   }
+
+  /**
+   * @returns {Promise<[IUser]> } Users Array.
+   */
 
   getUsersLevels() {
     return Users.getUsersLevels(this.baseURL);
